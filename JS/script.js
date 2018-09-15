6e8cb0a87a640807a09d63c514b3a4535099c025
Corné <cornebotma@hotmail.com>
2018-09-15 10:59:35 +0200

    Page fully responsive

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
 
     //Rotate toggle button on click
-    $("#toggle-button").click(function() {
-        if ($(this).css("transform") == 'none') {
-            $(this).css("transform", "rotate(180deg)");
-        } else {
-            $(this).css("transform", "");
-        }
-    })
+    $("#toggle-button").click(function(){
+        if (window.matchMedia("(min-width: 768px)").matches) {
+            $("#toggle-button").toggleClass("toggle-button-spin");
+        } else if (window.matchMedia("(max-width: 768px)").matches)  {
+            if ($("#toggle-button").css("transform") == "rotate(360deg)") {
+            console.log("werk")
+            //$("#toggle-button").css("transform", "rotate(90deg)");
+            }
+        } 
+    });
 
     //page sliding to form section
     $("#toggle-button").click(function(){
